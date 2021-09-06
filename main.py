de909166d6d4c55d8d42d9dfbdcee0a454a37c63
Malcolm Holmes <mdh@odoko.co.uk>
2021-09-06 18:18:17 +0100

    Initial version

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+from display import Display
+
+display = Display()
+
+display.show(0,'0')
+display.show(5,'5')
+display.show(10,':')
+display.show(13,'1')
+display.show(18,'2')  
+
+display.start()
+
+while True:
+    pass
