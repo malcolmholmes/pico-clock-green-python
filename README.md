a16c20b2bfbb5a9a8913db78ab3f1143d4e84617
Malcolm Holmes <mdh@odoko.co.uk>
2021-09-07 11:57:35 +0100

    Further progress

---
+++ README.md
@@ -0,0 +1,10 @@
+# MicroPython Pico-Clock-Green example
+
+First flash MicroPython onto the Pico.
+Then install `ampy` from AdaFruit.
+
+The code can then be executed with something like:
+```
+$ ampy --port /dev/ttyACM0 put display.py /display.py
+$ ampy --port /dev/ttyACM0 run main.py
+```
--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from display import Display
 
 display = Display()
 
-display.show(0,'0')
-display.show(5,'5')
-display.show(10,':')
-display.show(13,'1')
-display.show(18,'2')  
-
+# display.show(0,'0')
+# display.show(5,'5')
+# display.show(10,':')
+# display.show(13,'1')
+# display.show(18,'2')
+display.square()
+display.print()
 display.start()
 
 while True:
