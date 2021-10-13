aeca3d81afb0a1407093fe515a6f67cb5e80da79
Erik Sommer <ersotech@posteo.de>
2021-10-13 16:03:48 +0200

    display of day of the week is off

--- clock.py
+++ clock.py
@@ -35,5 +35,5 @@ class Clock(App):
     def update_time(self):
         t = self.rtc.get_time()
         now = "%02d:%02d" % (t[3], t[4])
-        self.display.show_day(t[6])
+        self.display.show_day((t[6] + 1) % 7)
         self.display.show_text(now)
